24fb994ac1ba0655300c66a25f87bd33fdd15630
CarlosGuerra1 <eugeniocarloz@gmail.com>
2021-06-03 13:14:05 -0500

    increm

--- src/callbacks.c
+++ src/callbacks.c
@@ -18,8 +18,8 @@
 
 #define IZQ		1
 #define DER		2
-#define ARR		3
-#define ABA		4
+#define ARRIBA	3
+#define ABAJO	4
 
 #define CRECE	1
 #define ACHICA	2
@@ -69,18 +69,15 @@ on_window1_destroy(GtkObject *object, gpointer user_data){
 
 gboolean on_window1_key_press_event (GtkWidget *widget, GdkEventKey *event, gpointer user_data){
 	GtkWidget *message_dialog;
-	message_dialog = gtk_message_dialog_new (user_data, GTK_DIALOG_MODAL, 
-											GTK_MESSAGE_ERROR, 
-											GTK_BUTTONS_OK, 
-											"Ya se murió, F"); 
+	message_dialog = gtk_message_dialog_new (user_data, GTK_DIALOG_MODAL, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, "Perdiste xP"); 
 	gtk_window_set_title(GTK_WINDOW(message_dialog), "Fin del juego");
 	
 	switch(event->keyval){
 	case GDK_Right:
 			if(band==0){
 				if(!MoverSerpiente(serpiente, DER, widget, tams)){
-				gtk_dialog_run(GTK_DIALOG(message_dialog));
-				gtk_widget_destroy(message_dialog);                              
+					gtk_dialog_run(GTK_DIALOG(message_dialog));
+					gtk_widget_destroy(message_dialog);                              
 				}
 			}
 			if (Comer(serpiente, tams)) {
@@ -92,8 +89,8 @@ gboolean on_window1_key_press_event (GtkWidget *widget, GdkEventKey *event, gpoi
 		case GDK_Left:
 			if(band==0){
 				if(!MoverSerpiente(serpiente, IZQ, widget, tams)){
-				gtk_dialog_run(GTK_DIALOG(message_dialog));
-				gtk_widget_destroy(message_dialog); 		
+					gtk_dialog_run(GTK_DIALOG(message_dialog));
+					gtk_widget_destroy(message_dialog); 		
 				}
 			}
 			if (Comer(serpiente, tams)) {
@@ -104,9 +101,9 @@ gboolean on_window1_key_press_event (GtkWidget *widget, GdkEventKey *event, gpoi
 		break;
 		case GDK_Up:
 			if(band==0){
-				if(!MoverSerpiente(serpiente, ARR, widget, tams)){
-				gtk_dialog_run(GTK_DIALOG(message_dialog));
-				gtk_widget_destroy(message_dialog); 
+				if(!MoverSerpiente(serpiente, ARRIBA, widget, tams)){
+					gtk_dialog_run(GTK_DIALOG(message_dialog));
+					gtk_widget_destroy(message_dialog); 
 				}
 			}
 			if (Comer(serpiente, tams)) {
@@ -117,13 +114,11 @@ gboolean on_window1_key_press_event (GtkWidget *widget, GdkEventKey *event, gpoi
 		break;
 		case GDK_Down:
 			if(band==0){
-				if(!MoverSerpiente(serpiente, ABA, widget, tams)){
-				gtk_dialog_run(GTK_DIALOG(message_dialog));
-				gtk_widget_destroy(message_dialog); 
-			}
-			}
-
-			
+				if(!MoverSerpiente(serpiente, ABAJO, widget, tams)){
+					gtk_dialog_run(GTK_DIALOG(message_dialog));
+					gtk_widget_destroy(message_dialog); 
+				}
+			}			
 			if (Comer(serpiente, tams)) {
 				serpiente = AjustarSerpiente(serpiente, &tams, com.tipo, widget);
 				com.tipo = NADA;
@@ -132,27 +127,25 @@ gboolean on_window1_key_press_event (GtkWidget *widget, GdkEventKey *event, gpoi
 		break;
 		default: break;
 	}
-  return FALSE;
+	return FALSE;
 }
 
 
-void
-on_nuevo1_activate(GtkMenuItem *menuitem, gpointer user_data){
+void on_nuevo1_activate(GtkMenuItem *menuitem, gpointer user_data){
     if(serpiente != NULL){
       free(serpiente);
       tams = 5;
       cuenta = 0;
-      serpiente = NuevaSerpiente(tams);
+      band = 0;
+      serpiente = NuevaSerpiente(tams);      
     }
 }
 
-void
-on_salir1_activate (GtkMenuItem *menuitem, gpointer user_data){
+void on_salir1_activate (GtkMenuItem *menuitem, gpointer user_data){
 	on_window1_destroy(menuitem, user_data);
 }
 
-void
-on_acerca_de1_activate (GtkMenuItem *menuitem, gpointer user_data){
+void on_acerca_de1_activate (GtkMenuItem *menuitem, gpointer user_data){
 
 }
 
@@ -160,26 +153,19 @@ gboolean on_drawingarea1_configure_event (GtkWidget *widget, GdkEventConfigure *
 	if(pixmap != NULL)
 		gdk_pixmap_unref(pixmap);
 	
-	pixmap = gdk_pixmap_new(widget->window,
-							widget->allocation.width,
-							widget->allocation.height,
-							-1);
+	pixmap = gdk_pixmap_new(widget->window, widget->allocation.width, widget->allocation.height, -1);
 	serpiente = NuevaSerpiente(tams);
-	DibujarSerpiente(widget);
-	
-  return TRUE;
+	DibujarSerpiente(widget);	
+	return TRUE;
 }
 
 gboolean on_drawingarea1_expose_event (GtkWidget *widget, GdkEventExpose  *event, gpointer user_data){
-  gdk_draw_pixmap(widget->window, 
-					widget->style->fg_gc[GTK_WIDGET_STATE(widget)],
-					pixmap,
-					event->area.x, event->area.y,
+	gdk_draw_pixmap(widget->window, widget->style->fg_gc[GTK_WIDGET_STATE(widget)], pixmap, 
+					event->area.x, event->area.y, 
 					event->area.x, event->area.y,
 					event->area.width, event->area.height);	
-  return FALSE;
+	return FALSE;
 }
-
 
 void DibujarSerpiente(GtkWidget * widget){
     GdkRectangle update_rect;
@@ -190,34 +176,28 @@ void DibujarSerpiente(GtkWidget * widget){
     if(font == NULL)
 		  font = gdk_font_load("fixed");
     
-    gdk_draw_rectangle(pixmap, widget->style->white_gc,
-              TRUE,
-              0, 0,
-              where_to_draw->allocation.width,
-              where_to_draw->allocation.height);    
-
-    //Inicio área de dibujo   
-    //DIbujar cola 
+    gdk_draw_rectangle(pixmap, widget->style->white_gc, TRUE, 0, 0, where_to_draw->allocation.width, where_to_draw->allocation.height);    
+              
     switch (serpiente[0].dir)
     {
       case DER:
-        gdk_draw_line(pixmap, widget->style->black_gc, 
-        serpiente[0].pos.x * TAMSERP + TAMSERP, 
-        serpiente[0].pos.y * TAMSERP, 
-        serpiente[0].pos.x * TAMSERP, 
-        serpiente[0].pos.y * TAMSERP + TAMSERP / 2);
-        
-        gdk_draw_line(pixmap, widget->style->black_gc, 
-        serpiente[0].pos.x * TAMSERP, 
-        serpiente[0].pos.y * TAMSERP + TAMSERP / 2, 
-        serpiente[0].pos.x * TAMSERP + TAMSERP, 
-        serpiente[0].pos.y * TAMSERP + TAMSERP);
+		gdk_draw_line(pixmap, widget->style->black_gc, 
+		serpiente[0].pos.x * TAMSERP + TAMSERP, 
+		serpiente[0].pos.y * TAMSERP, 
+		serpiente[0].pos.x * TAMSERP, 
+		serpiente[0].pos.y * TAMSERP + TAMSERP / 2);
 
-        gdk_draw_line(pixmap, widget->style->black_gc, 
-        serpiente[0].pos.x * TAMSERP + TAMSERP, 
-        serpiente[0].pos.y * TAMSERP + TAMSERP,
-        serpiente[0].pos.x * TAMSERP + TAMSERP,
-        serpiente[0].pos.y * TAMSERP);
+		gdk_draw_line(pixmap, widget->style->black_gc, 
+		serpiente[0].pos.x * TAMSERP, 
+		serpiente[0].pos.y * TAMSERP + TAMSERP / 2, 
+		serpiente[0].pos.x * TAMSERP + TAMSERP, 
+		serpiente[0].pos.y * TAMSERP + TAMSERP);
+
+		gdk_draw_line(pixmap, widget->style->black_gc, 
+		serpiente[0].pos.x * TAMSERP + TAMSERP, 
+		serpiente[0].pos.y * TAMSERP + TAMSERP,
+		serpiente[0].pos.x * TAMSERP + TAMSERP,
+		serpiente[0].pos.y * TAMSERP);
       break;
       case IZQ:
         gdk_draw_line(pixmap, widget->style->black_gc, 
@@ -238,7 +218,7 @@ void DibujarSerpiente(GtkWidget * widget){
         serpiente[0].pos.x * TAMSERP, 
         serpiente[0].pos.y * TAMSERP);
       break;
-      case ARR:
+      case ARRIBA:
         gdk_draw_line(pixmap, widget->style->black_gc, 
         serpiente[0].pos.x * TAMSERP,
         serpiente[0].pos.y * TAMSERP, 
@@ -257,7 +237,7 @@ void DibujarSerpiente(GtkWidget * widget){
         serpiente[0].pos.x * TAMSERP,
         serpiente[0].pos.y * TAMSERP);
       break;
-      case ABA:
+      case ABAJO:
         gdk_draw_line(pixmap, widget->style->black_gc, 
         serpiente[0].pos.x * TAMSERP,
         serpiente[0].pos.y * TAMSERP + TAMSERP, 
@@ -277,8 +257,8 @@ void DibujarSerpiente(GtkWidget * widget){
         serpiente[0].pos.y * TAMSERP + TAMSERP);
       break;
     }
-    
-    //Dibujar cuerpo
+
+	// CUERPO
     while (serpiente[i].tipo != CABEZA) {
       gdk_draw_rectangle(pixmap, 
               widget->style->black_gc,
@@ -290,7 +270,7 @@ void DibujarSerpiente(GtkWidget * widget){
       i++;
 	  }
     
-    //Dibujar cabeza
+    // CABEZA
     gdk_draw_rectangle(pixmap, 
               widget->style->black_gc,
               FALSE,
@@ -299,18 +279,15 @@ void DibujarSerpiente(GtkWidget * widget){
               TAMSERP,
               TAMSERP);
 
-    	switch (serpiente[i].dir)
-      {
+    switch (serpiente[i].dir){
       case DER:
-        gdk_draw_arc(pixmap, widget->style->black_gc, 
-          FALSE,
+        gdk_draw_arc(pixmap, widget->style->black_gc, FALSE,
           serpiente[i].pos.x * TAMSERP, 
           serpiente[i].pos.y * TAMSERP,
           TAMSERP / 2,
           TAMSERP / 2,
           0, (64*360));
-        gdk_draw_arc(pixmap, widget->style->black_gc, 
-          FALSE,
+        gdk_draw_arc(pixmap, widget->style->black_gc, FALSE,
           serpiente[i].pos.x * TAMSERP, 
           serpiente[i].pos.y * TAMSERP + TAMSERP / 2,
           TAMSERP / 2,
@@ -318,47 +295,41 @@ void DibujarSerpiente(GtkWidget * widget){
           0, (64*360));
         break;
       case IZQ:
-        gdk_draw_arc(pixmap, widget->style->black_gc, 
-          FALSE,
+        gdk_draw_arc(pixmap, widget->style->black_gc, FALSE,
           serpiente[i].pos.x * TAMSERP + TAMSERP / 2, 
           serpiente[i].pos.y * TAMSERP,
           TAMSERP / 2,
           TAMSERP / 2,
           0, (64*360));
-        gdk_draw_arc(pixmap, widget->style->black_gc, 
-          FALSE,
+        gdk_draw_arc(pixmap, widget->style->black_gc, FALSE,
           serpiente[i].pos.x * TAMSERP + TAMSERP / 2, 
           serpiente[i].pos.y * TAMSERP + TAMSERP / 2,
           TAMSERP / 2,
           TAMSERP / 2,
           0, (64*360));
         break;
-      case ARR:
-        gdk_draw_arc(pixmap, widget->style->black_gc, 
-          FALSE,
+      case ARRIBA:
+        gdk_draw_arc(pixmap, widget->style->black_gc, FALSE,
           serpiente[i].pos.x * TAMSERP, 
           serpiente[i].pos.y * TAMSERP + TAMSERP / 2,
           TAMSERP / 2,
           TAMSERP / 2,
           0, (64*360));
-        gdk_draw_arc(pixmap, widget->style->black_gc, 
-          FALSE,
+        gdk_draw_arc(pixmap, widget->style->black_gc, FALSE,
           serpiente[i].pos.x * TAMSERP + TAMSERP / 2, 
           serpiente[i].pos.y * TAMSERP + TAMSERP / 2,
           TAMSERP / 2,
           TAMSERP / 2,
           0, (64*360));
         break;
-      case ABA:
-        gdk_draw_arc(pixmap, widget->style->black_gc, 
-          FALSE,
+      case ABAJO:
+        gdk_draw_arc(pixmap, widget->style->black_gc, FALSE,
           serpiente[i].pos.x * TAMSERP, 
           serpiente[i].pos.y * TAMSERP,
           TAMSERP / 2,
           TAMSERP / 2,
           0, (64*360));
-        gdk_draw_arc(pixmap, widget->style->black_gc, 
-          FALSE,
+        gdk_draw_arc(pixmap, widget->style->black_gc, FALSE,
           serpiente[i].pos.x * TAMSERP + TAMSERP / 2, 
           serpiente[i].pos.y * TAMSERP,
           TAMSERP / 2,
@@ -366,22 +337,19 @@ void DibujarSerpiente(GtkWidget * widget){
           0, (64*360));
         break;
       }
-    //Fin área de dibujo
     
     
-    // Dibujar Comida
+    // COMIDA
     if(com.tipo== CRECE){
 		gdk_draw_rectangle(pixmap, 
-			  widget->style->black_gc,
-			  FALSE,
+			  widget->style->black_gc, FALSE,
 			  com.pos.x * TAMSERP,
 			  com.pos.y * TAMSERP,
 			  TAMSERP,
 			  TAMSERP);
 	
 	}else if(com.tipo == ACHICA){
-        gdk_draw_arc(pixmap, widget->style->black_gc, 
-          FALSE,
+        gdk_draw_arc(pixmap, widget->style->black_gc, FALSE,
           com.pos.x * TAMSERP, 
           com.pos.y * TAMSERP,
           TAMSERP,
@@ -390,8 +358,6 @@ void DibujarSerpiente(GtkWidget * widget){
 	
 	}
     
-    //MoverSerpiente(serpiente, serpiente[tams - 1].dir , widget, tams);
-    //DibujarSerpiente(widget);
     update_rect.x = 0;
     update_rect.y = 0;
     update_rect.width = where_to_draw->allocation.width;
@@ -406,13 +372,13 @@ PEDACITOS * NuevaSerpiente(int tams){
 	int i;
 	if (tams < 2)
 		tams = 2;
+		
 	serpiente = (PEDACITOS*)malloc(sizeof(PEDACITOS) * tams);
-	if (serpiente == NULL) {
-		//essageBox(NULL, L"Sin memoria", L"Error", MB_OK | MB_ICONERROR);
+	if (serpiente == NULL) {		
 		printf("Sin memoria");
     exit(0);
 	}
-  printf("Serpiente creada con exito");
+	printf("Serpiente Creada");
 	serpiente[0].tipo = COLA;
 	serpiente[0].pos.x = 1;
 	serpiente[0].pos.y = 1;
@@ -443,8 +409,7 @@ int MoverSerpiente(PEDACITOS* serpiente, int dir, GtkWidget * widget, int tams){
 		i++;
 	}
 
-	switch (serpiente[i].dir)
-	{
+	switch (serpiente[i].dir){
 	case DER:
 		if (dir != IZQ)
 			serpiente[i].dir = dir;
@@ -453,18 +418,17 @@ int MoverSerpiente(PEDACITOS* serpiente, int dir, GtkWidget * widget, int tams){
 		if (dir != DER)
 			serpiente[i].dir = dir;
 		break;
-	case ARR:
-		if (dir != ABA)
+	case ARRIBA:
+		if (dir != ABAJO)
 			serpiente[i].dir = dir;
 		break;
-	case ABA:
-		if (dir != ARR)
+	case ABAJO:
+		if (dir != ARRIBA)
 			serpiente[i].dir = dir;
 		break;
 	}
 
-	switch (serpiente[i].dir)
-	{
+	switch (serpiente[i].dir){
 	case DER:
 		serpiente[i].pos.x = serpiente[i].pos.x + 1;
 		if (serpiente[i].pos.x >= tablero->allocation.width / TAMSERP)
@@ -475,18 +439,17 @@ int MoverSerpiente(PEDACITOS* serpiente, int dir, GtkWidget * widget, int tams){
 		if (serpiente[i].pos.x < 0)
 			serpiente[i].pos.x = tablero->allocation.width / TAMSERP;
 		break;
-	case ARR:
+	case ARRIBA:
 		serpiente[i].pos.y = serpiente[i].pos.y - 1;
 		if (serpiente[i].pos.y < 0)
 			serpiente[i].pos.y = tablero->allocation.height / TAMSERP;
 		break;
-	case ABA:
+	case ABAJO:
 		serpiente[i].pos.y = serpiente[i].pos.y + 1;
 		if (serpiente[i].pos.y > tablero->allocation.height / TAMSERP)
 			serpiente[i].pos.y = 0;
 		break;
 	}
-
 	return !Colisionar(serpiente, tams);
 }
 
@@ -498,8 +461,7 @@ int Colisionar(PEDACITOS* serpiente, int tams){
 			return 1;
 		}
 		i++;
-	}
-	
+	}	
 	return 0;
 }
 
@@ -510,50 +472,49 @@ PEDACITOS * AjustarSerpiente(PEDACITOS* serpiente, int* tams, int comida, GtkWid
 	GtkWidget *tablero = lookup_widget(widget,"drawingarea1");
 	
 	switch (comida){
-	case CRECE: {
-	  (*tams)++;	  
-	  serpiente = (PEDACITOS *) realloc(serpiente, sizeof(PEDACITOS) * (*tams));
-	  serpiente[*tams - 2].tipo = CUERPO;
-	  serpiente[*tams - 1] = cabeza;
-	  i = *tams - 1;
-	  switch (serpiente[i].dir){
-	  case DER:
-		serpiente[i].pos.x = serpiente[i].pos.x + 1;
-		if (serpiente[i].pos.x >= tablero->allocation.width / TAMSERP)
-		  serpiente[i].pos.x = 0;
-		break;
-	  case IZQ:
-		serpiente[i].pos.x = serpiente[i].pos.x - 1;
-		if (serpiente[i].pos.x < 0)
-		  serpiente[i].pos.x = tablero->allocation.width / TAMSERP;
-		break;
-	  case ARR:
-		serpiente[i].pos.y = serpiente[i].pos.y - 1;
-		if (serpiente[i].pos.y < 0)
-		  serpiente[i].pos.y = tablero->allocation.height / TAMSERP;
-		break;
-	  case ABA:
-		serpiente[i].pos.y = serpiente[i].pos.y + 1;
-		if (serpiente[i].pos.y > tablero->allocation.height / TAMSERP)
-		  serpiente[i].pos.y = 0;
-		break;
-	  }
-	  break;
-	}
-	case ACHICA: {
-	  if (*tams > 2) {
-		
-		i = 0;
-		while (serpiente[i].tipo != CABEZA) {
-		  serpiente[i] = serpiente[i + 1];
-		  i++;
+		case CRECE: {
+			(*tams)++;	  
+			serpiente = (PEDACITOS *) realloc(serpiente, sizeof(PEDACITOS) * (*tams));
+			serpiente[*tams - 2].tipo = CUERPO;
+			serpiente[*tams - 1] = cabeza;
+			i = *tams - 1;
+			switch (serpiente[i].dir){
+				case DER:
+					serpiente[i].pos.x = serpiente[i].pos.x + 1;
+					if (serpiente[i].pos.x >= tablero->allocation.width / TAMSERP)
+					  serpiente[i].pos.x = 0;
+				break;
+				case IZQ:
+					serpiente[i].pos.x = serpiente[i].pos.x - 1;
+					if (serpiente[i].pos.x < 0)
+					  serpiente[i].pos.x = tablero->allocation.width / TAMSERP;
+				break;
+				case ARRIBA:
+					serpiente[i].pos.y = serpiente[i].pos.y - 1;
+					if (serpiente[i].pos.y < 0)
+					  serpiente[i].pos.y = tablero->allocation.height / TAMSERP;
+				break;
+				case ABAJO:
+					serpiente[i].pos.y = serpiente[i].pos.y + 1;
+					if (serpiente[i].pos.y > tablero->allocation.height / TAMSERP)
+					  serpiente[i].pos.y = 0;
+				break;
+			}
+		  break;
 		}
-		(*tams)--;
-		serpiente = (PEDACITOS*)realloc(serpiente, sizeof(PEDACITOS) * (*tams));
-		serpiente[*tams - 1] = cabeza;
-	  }
-	  break;
-	}
+		case ACHICA: {
+		  if (*tams > 2) {		
+			i = 0;
+			while (serpiente[i].tipo != CABEZA) {
+			  serpiente[i] = serpiente[i + 1];
+			  i++;
+			}
+			(*tams)--;
+			serpiente = (PEDACITOS*)realloc(serpiente, sizeof(PEDACITOS) * (*tams));
+			serpiente[*tams - 1] = cabeza;
+		  }
+		  break;
+		}
 	}
 	return serpiente;
 }
@@ -567,7 +528,6 @@ int Comer(PEDACITOS* serpiente, int tams){
 }
 
 void CrearComida(GtkWidget * widget){	
-	
 	DibujarSerpiente(widget);
 	GtkWidget *tablero = lookup_widget(widget,"drawingarea1");
 	if(rand() % 100 < 80){
@@ -578,10 +538,6 @@ void CrearComida(GtkWidget * widget){
 	com.pos.x = rand() % tablero->allocation.width / TAMSERP;
 	com.pos.y = rand() % tablero->allocation.height / TAMSERP;
 	DibujarSerpiente(widget);
-	/*
-	if(Comer(serpiente, tams)){
-		serpiente = AjustarSerpiente(serpiente,&tams,widget, tams);
-	}*/
 }
 
 void Trazar(GtkWidget * widget){
